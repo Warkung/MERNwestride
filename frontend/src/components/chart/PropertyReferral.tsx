037289ab47abdd37890efdf617edc41610df5c8d
Warkung <warkung@gmail.com>
2025-01-23 11:46:45 +0700

    Chart Success frontend

--- frontend/src/components/chart/PropertyReferral.tsx
+++ frontend/src/components/chart/PropertyReferral.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import { Box, Stack, Typography } from "@mui/material";
+import React from "react";
+import { propertyReferralsInfo } from "./ChartConfig";
+
+interface ProgressBarProps {
+  title: string;
+  percentage: number;
+  color: string;
+}
+
+const ProgressBar = ({ title, percentage, color }: ProgressBarProps) => {
+  return (
+    <Box>
+      <Stack
+        direction="row"
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        <Typography fontSize={16} fontWeight={500} color={"#11142d"}>
+          {title}
+        </Typography>
+        <Typography fontSize={16} fontWeight={500} color="#11142d">
+          {percentage}%
+        </Typography>
+      </Stack>
+      <Box
+        mt={2}
+        position={"relative"}
+        width={"100%"}
+        height={"8px"}
+        borderRadius={10}
+        bgcolor={"#f4f5f7"}
+      >
+        <Box
+          position={"absolute"}
+          width={`${percentage}%`}
+          height={"100%"}
+          borderRadius={10}
+          bgcolor={color}
+        ></Box>
+      </Box>
+    </Box>
+  );
+};
 
 function PropertyReferral() {
   return (
-    <div>PropertyReferral</div>
-  )
+    <Box
+      p={4}
+      bgcolor={"#fff"}
+      minWidth={"300px"}
+      display={"flex"}
+      flexDirection={"column"}
+      borderRadius={5}
+      boxShadow={"0 4px 12px 0 rgba(18,38,63,.125)"}
+    >
+      <Typography variant={"h6"} color="#11142d">
+        Property Referral
+      </Typography>
+      <Stack my={"20px"} direction={"column"} gap={4}>
+        {propertyReferralsInfo.map((bar,index) => {
+          return <ProgressBar key={index} {...bar} />;
+        })}
+      </Stack>
+    </Box>
+  );
 }
 
-export default PropertyReferral
+export default PropertyReferral;
--- frontend/src/components/chart/TotalRevenue.tsx
+++ frontend/src/components/chart/TotalRevenue.tsx
@@ -1,5 +1,8 @@
-import { Box, Typography } from "@mui/material";
+import { ArrowCircleUpRounded } from "@mui/icons-material";
+import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
+import ReactApexChart from "react-apexcharts";
+import { TotalRevenueOptions, TotalRevenueSeries } from "./ChartConfig";
 
 function TotalRevenue() {
   return (
@@ -10,13 +13,33 @@ function TotalRevenue() {
       borderRadius={5}
       flexDirection={"column"}
       display={"flex"}
+      boxShadow={"0 4px 12px 0 rgba(18,38,63,.125)"}
     >
       <Typography variant={"h6"} color="#11142d">
         Total Revenue
       </Typography>
-      <Typography m>
-        Total Revenue
-      </Typography>
+      <Stack my={"20px"} direction={"row"} gap={4} flexWrap={"wrap"}>
+        <Typography fontSize={28} fontWeight={700} color="#11142d">
+          $235,798
+        </Typography>
+        <Stack direction={"row"} alignItems={"center"} gap={1}>
+          <ArrowCircleUpRounded sx={{ color: "#4caf50" }} />
+          <Stack>
+            <Typography fontSize={12} color="#4caf50">
+              0.8%
+            </Typography>
+            <Typography fontSize={12} color="#4caf50">
+              Since last month
+            </Typography>
+          </Stack>
+        </Stack>
+      </Stack>
+      <ReactApexChart
+        series={TotalRevenueSeries}
+        type="bar"
+        height={310}
+        options={TotalRevenueOptions}
+      />
     </Box>
   );
 }
